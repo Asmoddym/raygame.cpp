099d45c372563546448c751b4698b13b66331e58
Asmoddym <asmoddym@gmail.com>
2025-04-16 18:42:54 +0200

    Modified windows script a little bit

--- build-VisualStudio2022.bat
+++ build-VisualStudio2022.bat
@@ -1,3 +1,9 @@
 cd build
 premake5.exe vs2022 || pause
 cd ../
+
+set configuration=Debug
+
+del Bin\%configuration%\raygame.exe
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=%configuration%
+call Bin\Debug\raygame.exe

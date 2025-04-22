916e15ba110279d59959c75419b6962c92d3eae5
Sylvain CHAUGNY <sylvain.chaugny@elevo.io>
2025-04-22 14:58:23 +0200

    Added premake export-compile-commands as submodule

--- build-VisualStudio2022.bat
+++ build-VisualStudio2022.bat
@@ -1,13 +1,13 @@
 cd build
 premake5.exe vs2022 || pause
+premake5.exe export-compile-commands
 cd ../
+copy compile_commands/debug_x86.json compile_commands.json
 
 set configuration=Debug
 
 del Bin\%configuration%\raygame.exe
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=%configuration%
-
-copy compile_commands/debug_x86.json compile_commands.json
 
 call Bin\Debug\raygame.exe
 
--- build-osx-linux.sh
+++ build-osx-linux.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-
-
 rm -rf bin/$TYPE/raygame
 RULE="gmake2"
 TYPE="Debug"
 
 pushd build
 [[ $OSTYPE == 'darwin'* ]] && ./premake5.osx $RULE || ./premake5 $RULE
+[[ $OSTYPE == 'darwin'* ]] && ./premake5.osx export-compile-commands || ./premake5 export-compile-commands
 popd
+cp ./compile_commands/debug_x86.json ./compile_commands.json
 make
 ./bin/$TYPE/raygame

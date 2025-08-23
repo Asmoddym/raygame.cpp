1462b73c911509406fa06822e766f04b187e8fa9
Sylvain Chaugny <sylvain.chaugny@elevo.io>
2025-08-23 19:21:15 +0200

    Add raycast

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,38 @@
+#include "Debug.h"
 #include "engine/macro.h"
+#include "raylib.h"
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 1280
+#define screenHeight 720
+
+int worldMap[mapWidth][mapHeight]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
 
 namespace rg {
   namespace system {
@@ -23,9 +57,156 @@ namespace rg {
           DrawText(Concatenate(GetFPS(), " FPS (", Timer::since(), "ms)").c_str(), 10, 10, 20, LIME);
           EndDrawing();
         }
+        double posX = 22, posY = 12;  //x and y start position
+        double dirX = -1, dirY = 0; //initial direction vector
+        double planeX = 0, planeY = 0.5; //the 2d raycaster version of camera plane
 
       private:
         inline void draw() {
+
+
+          double time = 0; //time of current frame
+          double oldTime = 0; //time of previous frame
+          for(int x = 0; x < screenWidth; x++) {
+
+            //calculate ray position and direction
+            double cameraX = 2 * x / double(screenWidth) - 1; //x-coordinate in camera space
+            double rayDirX = dirX + planeX * cameraX;
+            double rayDirY = dirY + planeY * cameraX;
+
+            //which box of the map we're in
+            int mapX = int(posX);
+            int mapY = int(posY);
+
+            //length of ray from current position to next x or y-side
+            double sideDistX;
+            double sideDistY;
+
+            //length of ray from one x or y-side to next x or y-side
+            double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+            double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+            double perpWallDist;
+
+            //what direction to step in x or y-direction (either +1 or -1)
+            int stepX;
+            int stepY;
+
+            int hit = 0; //was there a wall hit?
+            int side; //was a NS or a EW wall hit?
+                      //calculate step and initial sideDist
+            if (rayDirX < 0)
+            {
+              stepX = -1;
+              sideDistX = (posX - mapX) * deltaDistX;
+            }
+            else
+            {
+              stepX = 1;
+              sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+            }
+            if (rayDirY < 0)
+            {
+              stepY = -1;
+              sideDistY = (posY - mapY) * deltaDistY;
+            }
+            else
+            {
+              stepY = 1;
+              sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+            }
+
+            //perform DDA
+            while (hit == 0)
+            {
+              //jump to next map square, either in x-direction, or in y-direction
+              if (sideDistX < sideDistY)
+              {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+              }
+              else
+              {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+              }
+              //Check if ray has hit a wall
+              if (worldMap[mapX][mapY] > 0) hit = 1;
+            } 
+
+            //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
+            if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+            else          perpWallDist = (sideDistY - deltaDistY);
+
+            //Calculate height of line to draw on screen
+            int lineHeight = (int)(2 *screenHeight / perpWallDist);
+
+            //calculate lowest an highest pixel to fill in current stripe
+            int drawStart = -lineHeight / 2 +screenHeight / 2;
+            if(drawStart < 0)drawStart = 0;
+            int drawEnd = lineHeight / 2 +screenHeight / 2;
+            if(drawEnd >=screenHeight)drawEnd =screenHeight - 1;
+
+            //choose wall color
+            Color color;
+            switch (worldMap[mapX][mapY]) {
+              case 1:
+                color = RED;
+                break;
+              case 2:
+                color = BLUE;
+                break;
+              case 3:
+                color = YELLOW;
+                break;
+              case 4:
+                color = GREEN;
+                break;
+            }
+
+            if (side == 0) color.a /= 2;
+
+            DrawLine(x, drawStart, x, drawEnd, color); 
+          }
+
+          float moveSpeed = 0.15;
+          float rotSpeed = 0.03;
+          //move forward if no wall in front of you
+          if (IsKeyDown(KEY_UP))
+          {
+            if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
+            if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+          }
+          //move backwards if no wall behind you
+          if (IsKeyDown(KEY_DOWN))
+          {
+            if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
+            if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+          }
+          //rotate to the right
+          if (IsKeyDown(KEY_RIGHT))
+          {
+            //both camera direction and camera plane must be rotated
+            double oldDirX = dirX;
+            dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+            dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+            planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+          }
+          //rotate to the left
+          if (IsKeyDown(KEY_LEFT))
+          {
+            //both camera direction and camera plane must be rotated
+            double oldDirX = dirX;
+            dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+            dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+            planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+          }
+          DrawLine(100, 100, 100 - dirX * 20, 100 + dirY * 20, WHITE);
         }
     };
   }

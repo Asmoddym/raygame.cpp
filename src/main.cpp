1401ed7cefe0a5419e89ec08471ac66604049167
Sylvain Chaugny <sylvain.chaugny@elevo.io>
2025-08-24 10:28:11 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #define mapHeight 24
 #define screenWidth 1280
 #define screenHeight 720
+#define texWidth 64
+#define texHeight 64
 
 int worldMap[mapWidth][mapHeight]=
 {
@@ -75,6 +77,15 @@ namespace rg {
           m_camera.offset = ::Vector2 { 0, 0 };
           m_camera.rotation = 0.0f;
           m_camera.zoom = 1.0f;
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+          texture.emplace_back(LoadImageColors(LoadImage("pics/barrel.png")));
+
         };
 
         inline virtual void update() override {
@@ -85,14 +96,18 @@ namespace rg {
           EndMode2D();
           DrawText(Concatenate(GetFPS(), " FPS (", Timer::since(), "ms)").c_str(), 10, 10, 20, LIME);
           EndDrawing();
+
         }
         double posX = 22, posY = 12;  //x and y start position
         double dirX = -1, dirY = 0; //initial direction vector
         double planeX = 0, planeY = 0.5; //the 2d raycaster version of camera plane
 
+  int buffer[screenHeight][screenWidth]; // y-coordinate first because it works per scanline
+  std::vector<Color *> texture;
+
       private:
         inline void draw() {
-          drawLevel(1);
+          // drawLevel(1);
           drawLevel(0);
           float moveSpeed = 0.15;
           float rotSpeed = 0.03;
@@ -133,8 +148,8 @@ namespace rg {
           DrawLine(100, 100, 100 - dirX * 20, 100 + dirY * 20, WHITE);
         }
 
-          void drawLevel(int l) {
-            auto &level = l == 0 ? worldMap : worldMapLevel2;
+        void drawLevel(int l) {
+          auto &level = l == 0 ? worldMap : worldMapLevel2;
 
           for(int x = 0; x < screenWidth; x++) {
             //calculate ray position and direction
@@ -224,28 +239,69 @@ namespace rg {
               drawEnd -= lineHeight;
             }
 
+            //texturing calculations
+            int texNum = worldMap[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
 
-            //choose wall color
-            Color color;
-            switch (level[mapX][mapY]) {
-              case 1:
-                color = RED;
-                break;
-              case 2:
-                color = BLUE;
-                break;
-              case 3:
-                color = YELLOW;
-                break;
-              case 4:
-                color = GREEN;
-                break;
+            //calculate value of wallX
+            double wallX; //where exactly the wall was hit
+            if (side == 0) wallX = posY + perpWallDist * rayDirY;
+            else           wallX = posX + perpWallDist * rayDirX;
+            wallX -= floor((wallX));
+
+            //x coordinate on the texture
+            int texX = int(wallX * double(texWidth));
+            if(side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
+            if(side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
+
+
+
+            // How much to increase the texture coordinate per screen pixel
+            double step = 1.0 * texHeight / lineHeight;
+            // Starting texture coordinate
+            double texPos = (drawStart - screenHeight / 2 + lineHeight / 2) * step;
+            for(int y = drawStart; y<drawEnd; y++)
+            {
+              // Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
+              int texY = (int)texPos & (texHeight - 1);
+              texPos += step;
+
+              Color color = texture[texNum][texHeight * texY + texX];
+              if (side == 1) color.a /= 2;
+
+              // int color = ((int *)texture[texNum].data)[texHeight * texY + texX];
+
+              //make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+              // if(side == 1) color = (color >> 1) & 8355711;
+              // buffer[y][x] = color;
             }
-
-            if (side == 0) color.a /= 2;
-
-            DrawLine(x, drawStart, x, drawEnd, color); 
+            // for(int y = 0; y < screenHeight; y++) for(int x = 0; x < screenWidth; x++) buffer[y][x] = 0; //clear the buffer instead of cls()
           }
+
+
+
+
+
+            // //choose wall color
+            //
+            // Color color;
+            // switch (level[mapX][mapY]) {
+            //   case 1:
+            //     color = RED;
+            //     break;
+            //   case 2:
+            //     color = BLUE;
+            //     break;
+            //   case 3:
+            //     color = YELLOW;
+            //     break;
+            //   case 4:
+            //     color = GREEN;
+            //     break;
+            // }
+            //
+            // if (side == 0) color.a /= 2;
+            //
+            // DrawLine(x, drawStart, x, drawEnd, color); 
           }
     };
   }
